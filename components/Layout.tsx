d53e12665ee21cd4d43bbaa359c9dc673fb6942e
Aleksander M <a.mozhaev94@gmail.com>
2021-06-21 19:29:25 +0600

    update layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import Image from 'next/image'
 
 export default function layout({ children }: any): JSX.Element {
@@ -11,13 +12,23 @@ export default function layout({ children }: any): JSX.Element {
         <link rel="icon" type="image/svg" href="/favicon.svg" />
       </Head>
       <header className="container mx-auto">
-        <h1>Шапка</h1>
+        <div className="flex items-center">
+          <Link href="/"><a>
+            <Image src="/cats.svg"
+              width={200}
+              height={150} />
+          </a></Link>
+          <h1 className="font-sans">Cats world! Find the cat of your dreams</h1>
+        </div>
+        <div className="border-b-2"></div>
       </header>
       <main className="container mx-auto">
         {children}
       </main>
-      <footer>
-        <h1 className="container mx-auto">Футер</h1>
+      <footer className="container mx-auto">
+        <div className="border-t-2 mt-12">Хееей</div>
+        <h1>Футер</h1>
+        <div className="bg-catty-paws"></div>
       </footer>
     </>
   )
