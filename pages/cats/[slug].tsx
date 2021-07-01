661949b9f90edc674a2c4759055cbf0653a8dad0
Aleksander M <a.mozhaev94@gmail.com>
2021-07-01 18:14:47 +0600

    added new folder cats and dymanic router slug for view cat

---
+++ pages/cats/[slug].tsx
@@ -0,0 +1,38 @@
+import Layout from '../../components/Layout'
+import Image from 'next/image';
+interface Cat {
+  id: number,
+  name: string,
+  slug: string,
+  image_url: string,
+  description: string,
+  created_at: string,
+  updated_at: string,
+}
+
+export default function Cat(cat: Cat): JSX.Element {
+  return (
+    <>
+      <Layout title={cat.name}>
+        <div className="flex justify-center mt-8">
+          <Image src={cat.image_url} height={400} width={400}></Image>
+        </div>
+        <h1>{cat.name}</h1>
+        <h1>{cat.description}</h1>
+      </Layout>
+    </>
+  )
+}
+
+export async function getServerSideProps({ params }: any) {
+  const res = await fetch(`https://cats-api.strsqr.cloud/cats/${encodeURIComponent(params.slug)}`, {
+    method: 'GET',
+  })
+  const cat = await res.json()
+  return {
+    props: {
+      cat,
+    }
+  }
+}
+
