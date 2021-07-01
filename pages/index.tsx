ce7e40c2d5c75f2763a6262062053dd0c86e40ca
Aleksander M <a.mozhaev94@gmail.com>
2021-07-01 18:16:29 +0600

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,55 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Layout from '../components/Layout'
+import Card from '../components/Card'
+import Paginator from '../components/Paginator'
+import Searchbox from '../components/Searchbox'
 
-export default function Home() {
+interface CatsProps {
+  cats: Cats[],
+  countPages: number,
+  breedSlug: string,
+}
+interface Cats {
+  id: number,
+  name: string,
+  slug: string,
+  image_url: string,
+  created_at: string,
+  updated_at: string,
+}
+
+export default function Home({ cats, countPages }: CatsProps): JSX.Element {
   return (
-    <div className="container mx-auto">
-      <h1>Привет!</h1>
-    </div>
+    <Layout>
+      <div className="mt-8 ">
+        <h1 className="text-center text-4xl mb-8">Who will be your pet?</h1>
+      </div>
+      <Searchbox />
+      <div className="grid grid-cols-4 gap-4 mb-8">
+        {cats.map((cat) => (
+          <Card path={cat.image_url} name={cat.name} key={cat.id} slug={cat.slug}></Card>
+        ))}
+      </div>
+      <Paginator countPages={countPages}></Paginator>
+      <div className="mb-8"></div>
+    </Layout >
   )
+}
+
+
+export async function getServerSideProps(params: any) {
+  const parameter = params.query.p === undefined ? 1 : params.query.p;
+  const res = await fetch(`https://cats-api.strsqr.cloud/cats?p=${parameter}`, {
+    method: 'GET',
+  });
+  const countCats: number = Number(res.headers.get('cats-count'));
+  const cats = await res.json();
+  const countCatsInOnePage: number = (await (await fetch(`https://cats-api.strsqr.cloud/cats`)).json()).length;
+  const countPages: number = Math.ceil(countCats / countCatsInOnePage);
+
+  return {
+    props: {
+      cats,
+      countPages,
+    },
+  }
 }
