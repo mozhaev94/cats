f4f3d37c8ec0b063fd8970af148baad4103c0166
Aleksander M <a.mozhaev94@gmail.com>
2021-07-01 18:13:09 +0600

    fix component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,12 +1,15 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 interface CardProps {
   path: string;
   name: string;
+  slug: string;
 }
 
-export default function Card({ path, name }: CardProps): JSX.Element {
+export default function Card({ path, name, slug }: CardProps): JSX.Element {
+  const router = useRouter();
   return (
     <>
       <div className="border-2">
@@ -18,7 +21,7 @@ export default function Card({ path, name }: CardProps): JSX.Element {
           />
         </div>
         <div className="text-center w-94 mb-2 mt-2 text-xl">
-          <Link href="/"><a><h1>{name}</h1></a></Link>
+          <Link href={`/cats/${encodeURIComponent(slug)}`}><a><h1>{name}</h1></a></Link>
         </div>
       </div>
     </>
