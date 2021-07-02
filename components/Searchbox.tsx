cc729dc3c07822dca4596fb6c88fc0c02f00049b
Aleksander M <a.mozhaev94@gmail.com>
2021-07-03 01:45:45 +0600

    search without refresh page

--- components/Searchbox.tsx
+++ components/Searchbox.tsx
@@ -1,9 +1,20 @@
+import Link from 'next/link'
+import { useState } from "react";
+
 export default function Searchbox(): JSX.Element {
+
+  const [searchParam, setSearchParam] = useState('');
+
+  function handleChangeEvent(e: React.ChangeEvent<HTMLInputElement>) {
+    setSearchParam(e.target.value)
+  }
+
   return (
     <>
-      <form action="http://localhost:3000/search">
-        <div className="flex justify-end">
-          <input type="text" placeholder="Порода" className="border-2" name="q" />
+      <form>
+        <div className="flex justify-center mb-6">
+          <input type="text" placeholder="Порода" className="border-2 w-1/5 outline-none mr-4 p-2" name="q" onChange={handleChangeEvent} />
+          <Link href={`http://localhost:3000/search?q=${searchParam}`}><button type="submit" className="border-2 active: outline-none">SEARCH</button></Link>
         </div>
       </form>
     </>
