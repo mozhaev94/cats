db3bf1a7fff27b5cb949179b9646444f23bd2d40
Aleksander M <a.mozhaev94@gmail.com>
2021-07-09 18:13:34 +0600

    upd style Searchbox

--- components/Searchbox.tsx
+++ components/Searchbox.tsx
@@ -12,10 +12,8 @@ export default function Searchbox(): JSX.Element {
   return (
     <>
       <form>
-        <div className="flex justify-center mb-6">
-          <input type="text" placeholder="Порода" className="border-2 w-1/5 outline-none mr-4 p-2" name="q" onChange={handleChangeEvent} />
-          <Link href={`http://localhost:3000/search?q=${searchParam}`}><button type="submit" className="border-2 active: outline-none">SEARCH</button></Link>
-        </div>
+        <input type="text" placeholder="Порода..." className="border-2 w-2/3 outline-none mr-4 p-2 rounded-full bg-gray-100" name="q" onChange={handleChangeEvent} />
+        <Link href={`http://localhost:3000/search?q=${searchParam}`}><button type="submit" className="active: outline-none rounded-full"></button></Link>
       </form>
     </>
   )
