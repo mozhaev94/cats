4e5ce3d74d88f54b2fff7e673b67f489b769b6e6
Aleksander M <a.mozhaev94@gmail.com>
2021-07-09 18:10:21 +0600

    upd style Layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image'
+import Searchbox from '../components/Searchbox'
 
 export default function layout({ children, title = 'Cats World', description = 'This page about cats', image_url, }: any): JSX.Element {
   return (
@@ -16,27 +17,22 @@ export default function layout({ children, title = 'Cats World', description = '
         <meta property="og:image:height" content="504" />
         <link rel="icon" type="image/svg" href="/favicon.svg" />
       </Head>
-      <div className="bg-red-50">
-        <header className="container mx-auto pt-8">
-          <div className="flex items-center justify-between">
-            <Link href="/"><a>
-              <Image src="/favicon.svg"
-                width={140}
-                height={100} />
-            </a></Link>
-            <h1 className="font-sans text-4xl">Cats world! Find the cat of your dreams.</h1>
-            <div className="">
-              <div className="flex flex-col">
-                <h1 className="text-xl">Contacts :</h1>
-                <a href="tel: +74951234567" className="text-xl">+7 (495) 123-45-67</a>
-                <a href="mailto: catsfun@cats.ru" className="text-xl">catsfun@cats.ru</a>
-              </div>
-            </div>
-          </div>
-          <div className="border-b-4 mt-6"></div>
-        </header>
+      <header className="container mx-auto flex items-center mt-8">
+        <div>
+          <Link href="/"><a>
+            <Image src="/new-logo.svg"
+              width={70}
+              height={70} />
+          </a></Link>
+        </div>
+        <span className="text-2xl font-bold">Cat World</span>
+        <div className="ml-60 w-2/3">
+          <Searchbox />
+        </div>
+      </header>
+      <div>
       </div>
-      <main className="container mx-auto">
+      <main>
         {children}
       </main>
     </>
