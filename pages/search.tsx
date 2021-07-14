c81c9a320dc48c901884cdc9c361e2bc714e625c
Aleksander M <a.mozhaev94@gmail.com>
2021-07-14 20:46:23 +0600

    use api url from config

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,32 +1,32 @@
-import Layout from '../components/Layout'
-import Card from '../components/Card'
-import Paginator from '../components/Paginator'
-import Searchbox from '../components/Searchbox'
-import Image from 'next/image';
+import Layout from "../components/Layout";
+import Card from "../components/Card";
+import Paginator from "../components/Paginator";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const ITEMS_ON_PAGE_COUNT: number = 10;
 
 interface SearchProps {
   cats: Cats[]
   countPages: number,
+  parameter: number,
 }
 interface Cats {
   id: number,
   name: string,
   slug: string,
   image_url: string,
-  created_at: string,
-  updated_at: string,
 }
 
-export default function Search({ cats, countPages }: SearchProps) {
+export default function Search({ cats, countPages, parameter }: SearchProps) {
+  const router = useRouter();
   if (cats.length === 0) {
     return (
       <Layout>
         <div className="container mx-auto">
           <div className="mt-8 ">
             <div className="flex flex-col justify-center">
-              <h1 className='text-center text-4xl'>К сожалению ничего не нашлось :(</h1>
+              <h1 className="text-center text-4xl">К сожалению по вашему запросу "{router.query.q}" ничего не нашлось :(</h1>
               <Image
                 src="/sad.svg"
                 width={300}
@@ -47,7 +47,7 @@ export default function Search({ cats, countPages }: SearchProps) {
               <Card path={cat.image_url} name={cat.name} key={cat.id} slug={cat.slug}></Card>
             ))}
           </div>
-          <Paginator countPages={countPages}></Paginator>
+          <Paginator countPages={countPages} parameter={parameter}></Paginator>
         </div>
       </Layout >
   )
@@ -56,18 +56,19 @@ export default function Search({ cats, countPages }: SearchProps) {
 }
 
 export async function getServerSideProps(params: any) {
-  const parameter = params.query.p === undefined ? 1 : params.query.p;
+  const parameter = Number(params.query.p === undefined ? 1 : params.query.p);
   const searchParam = (params.query.q).toLowerCase().trim();
-  const res = await fetch(`https://cats-api.strsqr.cloud/cats?q=${encodeURIComponent(searchParam)}&p=${parameter}`, {
-    method: 'GET',
+  const res = await fetch(`${process.env.API_URL}?q=${encodeURIComponent(searchParam)}&p=${parameter}`, {
+    method: "GET",
   });
-  const countCats: number = Number(res.headers.get('cats-count'));
+  const countCats: number = Number(res.headers.get("cats-count"));
   const cats = await res.json();
   const countPages: number = Math.ceil(countCats / ITEMS_ON_PAGE_COUNT);
   return {
     props: {
       cats,
       countPages,
+      parameter,
     }
   }
 }
